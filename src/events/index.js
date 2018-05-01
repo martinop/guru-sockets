ceeccf3b8f4aae28935414af1a285010b9d43298
Martin Ocando <martinocandop@gmail.com>
2018-05-01 15:12:41 -0400

    Send message to room and save it in DB

--- src/events/index.js
+++ src/events/index.js
@@ -1,16 +1,30 @@
+import request from 'request-promise'
+
 export default (io) => {
     io.on('connection', (socket) => {
         socket.on('join', (data) => {
-            console.log(data)
+            const { id_subject } = JSON.parse(data)
+            socket.join(id_subject)
         })
 
         socket.on('message', (data) => {
-            console.log(data)
+            const { id_subject, username, id_user, message } = JSON.parse(data)
+            io.in(id_subject).emit('message', { message, username, id_user: id_user.toString() })
+            request('http://localhost:59954/messages', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    message, id_subject, id_user
+                })
+            })
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
         })
 
         socket.on('disconnect', () => {
             console.log(`Disconnected: ${socket.id}`)
-            Presence.remove(socket.id)
         })
     })
 }
